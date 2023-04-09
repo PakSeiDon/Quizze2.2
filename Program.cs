723e4b85ad30fae5aa588d2dd5f9ff5299568b84
elena <elena@DESKTOP-PG6RJ3Q>
2023-04-09 20:33:12 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queez2_2
+
+//დაწერეთ პროგრამა რომელიც შეადარებს ორ მასივს და იტყვის არის თუ არა ეს ორი მასივი ტოლი... 
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = new int[7];
+            int[] myarray1 = new int[12];
+            int[] myarray2 = new int[12];
+            int[] myarray3 = new int[16];
+            int[] myarray5 = new int[5];
+
+
+            bool isEqual = myarray.SequenceEqual(myarray1);
+
+            Console.WriteLine(isEqual);
+            {
+                bool isEqual2 = myarray5.SequenceEqual(myarray2);
+
+                Console.WriteLine(isEqual2);
+            }
+
+            {
+                bool isEqual3 = myarray1.SequenceEqual(myarray2);
+
+                Console.WriteLine(isEqual3);
+
+            }
+
+        }
+    }
+}
+}
